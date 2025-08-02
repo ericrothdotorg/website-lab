714eee742da5f00f595ab5caae1ae7ad6bed576a
Eric Roth <eric.roth@outlook.com>
2025-08-02 08:22:12 +0200

    Update language-switcher.php
    Added some languages

--- overall/language-switcher.php
+++ overall/language-switcher.php
@@ -7,7 +7,7 @@ add_action('wp_footer', function () {
         <span id="reset-language" title="Reset Language" role="button" aria-label="Reset Language" tabindex="0"
             onclick="resetTranslation()" 
             onkeydown="if(event.key === 'Enter' || event.key === ' ') resetTranslation();" 
-            style="margin-right: 7.5px;">
+            style="margin-right: 8px;">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#FFFFFF" viewBox="0 0 24 24" style="cursor: pointer;">
                 <title>Reset Language</title>
                 <path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6s-2.69 6-6 6a6.003 6.003 0 0 1-5.65-4H4.26a8.003 8.003 0 0 0 15.47-2c0-4.42-3.58-8-8-8z"/>
@@ -16,17 +16,21 @@ add_action('wp_footer', function () {
         <img src="https://flagcdn.com/w40/sa.png" alt="العربية" title="العربية" role="button" tabindex="0" lang="ar" onclick="handleFlagClick('ar')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('ar');">
         <img src="https://flagcdn.com/w40/id.png" alt="Bahasa" title="Bahasa" role="button" tabindex="0" lang="id" onclick="handleFlagClick('id')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('id');">
         <img src="https://flagcdn.com/w40/cn.png" alt="中文" title="中文" role="button" tabindex="0" lang="zh-CN" onclick="handleFlagClick('zh-CN')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('zh-CN');">
+        <img src="https://flagcdn.com/w40/dk.png" alt="Dansk" title="Danish" role="button" tabindex="0" lang="da" onclick="handleFlagClick('da')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('da');">
         <img src="https://flagcdn.com/w40/de.png" alt="Deutsch" title="Deutsch" role="button" tabindex="0" lang="de" onclick="handleFlagClick('de')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('de');">
-        <img src="https://flagcdn.com/w40/us.png" alt="English" title="English" role="button" tabindex="0" lang="en" onclick="handleFlagClick('en')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('en');">
         <img src="https://flagcdn.com/w40/es.png" alt="Español" title="Español" role="button" tabindex="0" lang="es" onclick="handleFlagClick('es')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('es');">
         <img src="https://flagcdn.com/w40/fr.png" alt="Français" title="Français" role="button" tabindex="0" lang="fr" onclick="handleFlagClick('fr')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('fr');">
         <img src="https://flagcdn.com/w40/in.png" alt="हिन्दी" title="हिन्दी" role="button" tabindex="0" lang="hi" onclick="handleFlagClick('hi')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('hi');">
         <img src="https://flagcdn.com/w40/it.png" alt="Italiano" title="Italiano" role="button" tabindex="0" lang="it" onclick="handleFlagClick('it')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('it');">
         <img src="https://flagcdn.com/w40/jp.png" alt="日本語" title="日本語" role="button" tabindex="0" lang="ja" onclick="handleFlagClick('ja')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('ja');">
         <img src="https://flagcdn.com/w40/kr.png" alt="한국어" title="한국어" role="button" tabindex="0" lang="ko" onclick="handleFlagClick('ko')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('ko');">
+        <img src="https://flagcdn.com/w40/no.png" alt="Norsk" title="Norwegian" role="button" tabindex="0" lang="no" onclick="handleFlagClick('no')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('no');">
+        <img src="https://flagcdn.com/w40/fi.png" alt="Suomi" title="Finnish" role="button" tabindex="0" lang="fi" onclick="handleFlagClick('fi')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('fi');">
+        <img src="https://flagcdn.com/w40/se.png" alt="Svenska" title="Swedish" role="button" tabindex="0" lang="sv" onclick="handleFlagClick('sv')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('sv');">
         <img src="https://flagcdn.com/w40/ph.png" alt="Tagalog" title="Tagalog" role="button" tabindex="0" lang="tl" onclick="handleFlagClick('tl')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('tl');">
         <img src="https://flagcdn.com/w40/th.png" alt="ไทย" title="ไทย" role="button" tabindex="0" lang="th" onclick="handleFlagClick('th')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('th');">
         <img src="https://flagcdn.com/w40/tr.png" alt="Türkçe" title="Türkçe" role="button" tabindex="0" lang="tr" onclick="handleFlagClick('tr')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('tr');">
+        <img src="https://flagcdn.com/w40/vn.png" alt="Tiếng Việt" title="Vietnamese" role="button" tabindex="0" lang="vi" onclick="handleFlagClick('vi')" onkeydown="if(event.key === 'Enter' || event.key === ' ') handleFlagClick('vi');">
     </div>
     <div id="language-toggle" role="button" aria-label="Open Language Switcher" tabindex="0"
         onclick="toggleLanguageFlags()"
