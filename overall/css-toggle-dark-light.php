5e461cf7b20c0815db9ef8c68af22c7b2b0387c0
Eric Roth <eric.roth@outlook.com>
2026-05-31 07:22:47 +0200

    Update css-toggle-dark-light.php

--- overall/css-toggle-dark-light.php
+++ overall/css-toggle-dark-light.php
@@ -607,11 +607,6 @@ add_action("wp_footer", function () {
 	@media (min-width: 768px) {.hide-on-big {display: none;}}
 	.flex-item-country-icons {flex: 0 0 60px;} /* Used for Photo Album */
 	
-	/* INJECTED STUFF */
-	.search-pattern-inject  { margin-top: -125px; margin-bottom: 25px; } /* Inject 'Random Content (EN)' in Search Page */
-	.home-pattern-inject    { margin-top: -125px; margin-bottom: 25px; } /* Inject 'Random Content (EN)' in Posts Page */
-	.archive-pattern-inject { margin-top: -25px;  margin-bottom: 25px; } /* Inject 'Random Content (EN)' in (Pseudo-)Archive Page */
-	
 	/* FOOTER */
 	
 	/* Radius on Hover */
