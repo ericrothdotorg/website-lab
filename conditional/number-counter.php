d82bd5e65d54e86792da128cb5f53d45951ae8da
Eric Roth <eric.roth@outlook.com>
2026-07-05 10:03:13 +0200

    Enhance counter card styles and dark mode support
    Added transform effect to counter card body and adjusted styles for dark mode.

--- conditional/number-counter.php
+++ conditional/number-counter.php
@@ -24,26 +24,31 @@ add_action('wp_head', function() use ($counter_pages) {
     if (!is_page($counter_pages)) return;
     ?>
     <style>
-        :root {--disc-size: 185px;}
+		:root {--disc-size: 185px;}
         .counter-grid {display: grid; grid-template-columns: repeat(6, 1fr); gap: 10px; justify-content: center;}
         .counter-card {margin: 10px auto; border-radius: 50%; display: flex; align-items: center; justify-content: center; width: var(--disc-size); max-width: 100%; aspect-ratio: 1 / 1; transition: transform 1.5s ease-in-out;}
         .counter-card:hover {transform: scale(1.05); background: var(--color-7);}
-        .counter-card .counter-body {text-align: center; color: var(--color-3);}
+        .counter-card .counter-body {text-align: center; color: var(--color-3); transform: translateY(-5px);}
+        .counter-body img {margin-bottom: 0.25em;}
         .counter-value {color: #990033; font-size: var(--er-fs-xl); font-weight: var(--er-fw-bold);}
         .counter-value, .counter-label {vertical-align: middle;}
         .counter-label {padding-left: 10px; font-size: var(--er-fs-md); font-weight: var(--er-fw-normal);}
-		
+
         @media (max-width: 1200px) {.counter-grid {grid-template-columns: repeat(auto-fit, minmax(var(--disc-size), 1fr));}}
-		@media (max-width: 600px) {:root {--disc-size: 160px;} .counter-grid {grid-template-columns: repeat(2, 1fr);} .counter-value {font-size: var(--er-fs-lg);} .counter-label {font-size: 1rem;}}
-		
-		body.dark-mode .counter-card {border: 1px solid var(--color-4); background: var(--color-10);}
-		body.dark-mode .counter-card:hover {background: var(--color-6);}
-		body.dark-mode .counter-body {color: var(--color-5);}
-		
-        /*Page specific*/
+        @media (max-width: 600px) {:root {--disc-size: 160px;} .counter-grid {grid-template-columns: repeat(2, 1fr);} .counter-value {font-size: var(--er-fs-lg);} .counter-label {font-size: 1rem;}}
+
+        body.dark-mode .counter-card {border: 1px solid var(--color-4); background: var(--color-10);}
+        body.dark-mode .counter-card:hover {background: var(--color-6);}
+        body.dark-mode .counter-body {color: var(--color-5);}
+
+        /* Page Specific */
         .page-id-179 .counter-card {background: rgba(0, 0, 0, 0.65) !important; border: none !important;}
         .page-id-179 .counter-card:hover {background: var(--color-6) !important;}
         .page-id-179 .counter-body {color: var(--color-5);}
+
+        /* White Icons: Dark Mode + Frontpage */
+        body.dark-mode .counter-body img,
+        .page-id-179 .counter-body img {filter: brightness(0) invert(1) brightness(0.95) !important;}
     </style>
     <?php
 }, 5);
